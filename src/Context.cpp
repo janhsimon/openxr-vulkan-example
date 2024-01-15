af5fea23fbb48ab33e9f34e228a400bc4fdcf59d
janhsimon <janhsimon@gmail.com>
2024-01-15 21:05:08 +0100

    Fix type error for Vulkan debug messenger flags

--- src/Context.cpp
+++ src/Context.cpp
@@ -444,7 +444,7 @@ Context::Context()
       return;
     }
 
-    constexpr XrDebugUtilsMessageTypeFlagsEXT typeFlags = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+    constexpr VkDebugUtilsMessageTypeFlagsEXT typeFlags = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
                                                           VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
                                                           VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 
